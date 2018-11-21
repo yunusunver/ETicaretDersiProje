db6f538639ba262d42c622a919f5f2dd93c50e07
yunusunver <yunus.unver1995@gmail.com>
2018-11-21 18:44:26 +0300

    Case5:Repository ve IRepository Sınıflarının Oluşturulması

---
+++ ETicaretDersiProje.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretDersiProje.Core.Entities;
+
+namespace ETicaretDersiProje.Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity,TContext>:IEntityRepository<TEntity>
+        where TEntity:class ,IEntity,new()
+        where TContext:DbContext,new()
+    {
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var context=new TContext())
+            {
+                return filter == null
+                    ? context.Set<TEntity>().ToList()
+                    : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (var context=new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            using (var context=new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+                return entity;
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ETicaretDersiProje.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretDersiProje.Core.Entities;
+
+namespace ETicaretDersiProje.Core.DataAccess
+{
+    public interface IEntityRepository<T> where T:class ,IEntity,new()
+    {
+        List<T> GetList(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T,bool>> filter);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+    }
+}
