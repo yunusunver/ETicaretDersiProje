dc630208055bc7fd42702e5067521c6fb7b5a599
yunusunver <yunus.unver1995@gmail.com>
2018-11-24 18:52:39 +0300

    Case 13:Validation Hatalarını Döndüren Sınıf

---
+++ ETicaretDersiProje.Eticaret.Business/CrossCuttingConcerns/Validation/FluentValidation/ValidatorTools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace ETicaretDersiProje.Eticaret.Business.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public class ValidatorTools
+    {
+        public static void FluentValidate(IValidator validator, object entity)
+        {
+            var result = validator.Validate(entity);
+            if (result.Errors.Count>0)
+            {
+                throw  new ValidationException(result.Errors);
+            }
+        }
+    }
+}
