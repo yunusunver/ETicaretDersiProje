a638ee6a949c349cd1c0ee87bb9288e4ff47cec9
yunusunver <yunus.unver1995@gmail.com>
2018-11-25 15:27:03 +0300

    Case 16:Ninject ile Validation için Dependency İnjection Tanımlanması

---
+++ ETicaretDersiProje.Eticaret.Business/DependencyResolvers/Ninject/ValidationModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretDersiProje.Eticaret.Business.ValidationRules.FluentValidation;
+using ETicaretDersiProje.Eticaret.Entities.Concrete;
+using FluentValidation;
+using Ninject.Modules;
+
+namespace ETicaretDersiProje.Eticaret.Business.DependencyResolvers.Ninject
+{
+    public class ValidationModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IValidator<Category>>().To<CategoryValidatior>().InSingletonScope();
+            Bind<IValidator<Product>>().To<ProductValidatior>().InSingletonScope();
+            Bind<IValidator<Color>>().To<ColorValidatior>().InSingletonScope();
+            Bind<IValidator<Size>>().To<SizeValidatior>().InSingletonScope();
+            Bind<IValidator<Customer>>().To<CustomerValidatior>().InSingletonScope();
+            Bind<IValidator<Shipper>>().To<ShipperValidatior>().InSingletonScope();
+            Bind<IValidator<Supplier>>().To<SupplierValidatior>().InSingletonScope();
+        }
+    }
+}
