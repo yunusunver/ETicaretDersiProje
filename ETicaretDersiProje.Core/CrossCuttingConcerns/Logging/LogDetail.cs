fd950a4484f7c6a08c5fae717cd11d69a1617c31
yunusunver <yunus.unver1995@gmail.com>
2018-12-09 15:47:13 +0300

    Case 22: LogDetail Sınıfını Ekledim

---
+++ ETicaretDersiProje.Core/CrossCuttingConcerns/Logging/LogDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretDersiProje.Core.CrossCuttingConcerns.Logging
+{
+    public class LogDetail
+    {
+        public string FullName { get; set; }
+        public string MethodName { get; set; }
+        public List<LogParameter> LogParameters { get; set; }
+    }
+}
