b0d11e3b70e06001954a1713dc7e693755d71dbf
yunusunver <yunus.unver1995@gmail.com>
2018-12-09 16:04:07 +0300

    Case 23: LoggerService Sınıfının Oluşturulması

---
+++ ETicaretDersiProje.Core/CrossCuttingConcerns/Logging/Log4Net/LoggerService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace ETicaretDersiProje.Core.CrossCuttingConcerns.Logging.Log4Net
+{
+    public class LoggerService
+    {
+        private ILog _log;
+
+        public LoggerService(ILog log)
+        {
+            _log = log;
+        }
+
+        public bool IsInfoEnabled => _log.IsInfoEnabled;
+        public bool IsDebugEnabled => _log.IsDebugEnabled;
+        public bool IsWarnEnabled => _log.IsWarnEnabled;
+        public bool IsFatalEnabled => _log.IsFatalEnabled;
+        public bool IsErrorEnabled => _log.IsErrorEnabled;
+
+
+        public void Info(object logMessage)
+        {
+            if (IsInfoEnabled)
+            {
+                 _log.Info(logMessage);
+            }
+        }
+
+        public void Debug(object logMessage)
+        {
+            if (IsDebugEnabled)
+            {
+                _log.Debug(logMessage);
+            }
+        }
+
+        public void Warn(object logMessage)
+        {
+            if (IsWarnEnabled)
+            {
+                _log.Warn(logMessage);
+            }
+        }
+
+        public void Fatal(object logMessage)
+        {
+            if (IsFatalEnabled)
+            {
+                _log.Fatal(logMessage);
+            }
+        }
+
+        public void Error(object logMessage)
+        {
+            if (IsErrorEnabled)
+            {
+                _log.Error(logMessage);
+            }
+        }
+
+
+    }
+}
