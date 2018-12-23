d49537e7a12d867e2ab1545ac8b7e65c0d9fe52d
yunusunver <yunus.unver1995@gmail.com>
2018-12-24 00:45:40 +0300

    Admin İşlemleri: Case : 5,6,7,48

--- ETicaretDersiProje.Eticaret.MvcWebUI/Controllers/CategoryController.cs
+++ ETicaretDersiProje.Eticaret.MvcWebUI/Controllers/CategoryController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using ETicaretDersiProje.Eticaret.Business.Abstract;
+using ETicaretDersiProje.Eticaret.Entities.Concrete;
 using ETicaretDersiProje.Eticaret.MvcWebUI.Models;
 
 namespace ETicaretDersiProje.Eticaret.MvcWebUI.Controllers
@@ -26,5 +29,73 @@ namespace ETicaretDersiProje.Eticaret.MvcWebUI.Controllers
 
             return View(model);
         }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category,HttpPostedFileBase image)
+        {
+            if (image != null)
+            {
+                WebImage img = new WebImage(image.InputStream);
+                FileInfo fotoInfo = new FileInfo(image.FileName);
+
+                string newFoto = Guid.NewGuid().ToString() + fotoInfo.Extension;
+                img.Resize(800, 350);
+                img.Save("~/Uploads/Category/" + newFoto);
+                category.Picture = "/Uploads/Category/" + newFoto;
+            }
+
+            category.Active = true;
+            _categoryService.Add(category);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var category = _categoryService.GetbyId(id);
+
+            if (System.IO.File.Exists(Server.MapPath(category.Picture)))
+            {
+                System.IO.File.Delete(Server.MapPath(category.Picture));
+            }
+            _categoryService.Delete(category);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var category = _categoryService.GetbyId(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category,HttpPostedFileBase image)
+        {
+            if (image != null)
+            {
+                if (System.IO.File.Exists(Server.MapPath(category.Picture)))
+                {
+                    System.IO.File.Delete(Server.MapPath(category.Picture));
+                }
+                WebImage img = new WebImage(image.InputStream);
+                FileInfo fotoInfo = new FileInfo(image.FileName);
+
+                string newFoto = Guid.NewGuid().ToString() + fotoInfo.Extension;
+                img.Resize(800, 350);
+                img.Save("~/Uploads/Category/" + newFoto);
+                category.Picture = "/Uploads/Category/" + newFoto;
+                category.Active = true;
+                _categoryService.Update(category);
+            }
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
