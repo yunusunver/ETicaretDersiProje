ef533c04269cd8c895695d1629fcaf349464a19a
yunusunver <yunus.unver1995@gmail.com>
2018-12-25 00:43:59 +0300

    Color İşlemleri Case:23,24,25,26

---
+++ ETicaretDersiProje.Eticaret.MvcWebUI/Controllers/ColorController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ETicaretDersiProje.Eticaret.Business.Abstract;
+using ETicaretDersiProje.Eticaret.Entities.Concrete;
+using ETicaretDersiProje.Eticaret.MvcWebUI.Models;
+
+namespace ETicaretDersiProje.Eticaret.MvcWebUI.Controllers
+{
+    public class ColorController : Controller
+    {
+        private IColorService _colorService;
+
+        public ColorController(IColorService colorService)
+        {
+            _colorService = colorService;
+        }
+
+        // GET: Color
+        public ActionResult Index()
+        {
+            ColorListViewModel model=new ColorListViewModel()
+            {
+                Colors = _colorService.GetAll()
+            };
+            return View(model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var color = _colorService.GetbyId(id);
+            _colorService.Delete(color);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Color color)
+        {
+            _colorService.Add(color);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var color = _colorService.GetbyId(id);
+            return View(color);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Color color)
+        {
+            _colorService.Update(color);
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
---
+++ ETicaretDersiProje.Eticaret.MvcWebUI/Models/ColorListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ETicaretDersiProje.Eticaret.Entities.Concrete;
+
+namespace ETicaretDersiProje.Eticaret.MvcWebUI.Models
+{
+    public class ColorListViewModel
+    {
+        public List<Color> Colors;
+    }
+}
