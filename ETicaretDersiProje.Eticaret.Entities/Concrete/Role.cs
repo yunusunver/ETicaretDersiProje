65add1fca542779f95d6157dae3c7174c0c79507
yunusunver <yunus.unver1995@gmail.com>
2018-12-25 14:35:56 +0300

    Role İşlemleri Case: 12,13,14,50

--- ETicaretDersiProje.Eticaret.Entities/Concrete/Role.cs
+++ ETicaretDersiProje.Eticaret.Entities/Concrete/Role.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace ETicaretDersiProje.Eticaret.Entities.Concrete
 {
     public class Role:IEntity
     {
+        
         public int Id { get; set; }
         public string RoleName { get; set; }
 
---
+++ ETicaretDersiProje.Eticaret.MvcWebUI/Controllers/RoleController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ETicaretDersiProje.Eticaret.Business.Abstract;
+using ETicaretDersiProje.Eticaret.Entities.Concrete;
+using ETicaretDersiProje.Eticaret.MvcWebUI.Models;
+
+namespace ETicaretDersiProje.Eticaret.MvcWebUI.Controllers
+{
+    public class RoleController : Controller
+    {
+        private IRoleService _roleService;
+
+        public RoleController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        public ActionResult Index()
+        {
+            RoleListViewModel model=new RoleListViewModel()
+            {
+                Roles = _roleService.GetAll()
+            };
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Role role)
+        {
+            _roleService.Add(role);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var role = _roleService.GetbyId(id);
+            return View(role);
+        }
+        [HttpPost]
+        public ActionResult Edit(Role role)
+        {
+            _roleService.Update(role);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var role = _roleService.GetbyId(id);
+            _roleService.Delete(role);
+            return RedirectToAction("Index");
+        }
+    }
+}
