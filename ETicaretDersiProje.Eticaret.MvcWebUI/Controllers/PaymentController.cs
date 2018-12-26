86097f618a14e8500eeb889a62f94f8772a821b0
yunusunver <yunus.unver1995@gmail.com>
2018-12-27 02:39:23 +0300

    Payments İşlemleri Case:32,33,34,35

---
+++ ETicaretDersiProje.Eticaret.MvcWebUI/Controllers/PaymentController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ETicaretDersiProje.Eticaret.Business.Abstract;
+using ETicaretDersiProje.Eticaret.Entities.Concrete;
+using ETicaretDersiProje.Eticaret.MvcWebUI.Models;
+
+namespace ETicaretDersiProje.Eticaret.MvcWebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private IPaymentService _paymentService;
+
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        // GET: Payment
+        public ActionResult Index()
+        {
+            PaymentListViewModel model=new PaymentListViewModel()
+            {
+                Payments = _paymentService.GetAll()
+            };
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Payment payment)
+        {
+            _paymentService.Add(payment);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var payment = _paymentService.GetbyId(id);
+
+            return View(payment);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Payment payment)
+        {
+            _paymentService.Update(payment);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var payment = _paymentService.GetbyId(id);
+            _paymentService.Delete(payment);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ETicaretDersiProje.Eticaret.MvcWebUI/Models/PaymentListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ETicaretDersiProje.Eticaret.Entities.Concrete;
+
+namespace ETicaretDersiProje.Eticaret.MvcWebUI.Models
+{
+    public class PaymentListViewModel
+    {
+        public List<Payment> Payments { get; set; }
+    }
+}
