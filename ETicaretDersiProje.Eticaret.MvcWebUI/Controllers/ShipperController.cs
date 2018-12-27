1bc36602cd19826c9d9ba313429783330e9a158f
yunusunver <yunus.unver1995@gmail.com>
2018-12-27 03:02:21 +0300

    Shipper İşlemleri Case:27,28,30,31

---
+++ ETicaretDersiProje.Eticaret.MvcWebUI/Controllers/ShipperController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ETicaretDersiProje.Eticaret.Business.Abstract;
+using ETicaretDersiProje.Eticaret.Entities.Concrete;
+using ETicaretDersiProje.Eticaret.MvcWebUI.Models;
+
+namespace ETicaretDersiProje.Eticaret.MvcWebUI.Controllers
+{
+    public class ShipperController : Controller
+    {
+        private IShipperService _shipperService;
+
+        public ShipperController(IShipperService shipperService)
+        {
+            _shipperService = shipperService;
+        }
+
+        // GET: Shipper
+        public ActionResult Index()
+        {
+            ShipperListViewModel model=new ShipperListViewModel()
+            {
+                Shippers = _shipperService.GetAll()
+            };
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Shipper shipper)
+        {
+            _shipperService.Add(shipper);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var shipper = _shipperService.GetbyId(id);
+            return View(shipper);
+        }
+        [HttpPost]
+        public ActionResult Edit(Shipper shipper)
+        {
+            _shipperService.Update(shipper);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var shipper = _shipperService.GetbyId(id);
+            _shipperService.Delete(shipper);
+            return RedirectToAction("Index");
+        }
+    }
+}
