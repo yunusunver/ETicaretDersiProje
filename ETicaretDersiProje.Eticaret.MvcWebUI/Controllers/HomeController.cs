994d590b2f9f706542f21e59e329a0d6949ac15d
yunusunver <yunus.unver1995@gmail.com>
2019-01-08 18:16:19 +0300

    Case: 10

--- ETicaretDersiProje.Eticaret.MvcWebUI/Controllers/HomeController.cs
+++ ETicaretDersiProje.Eticaret.MvcWebUI/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -145,6 +148,43 @@ namespace ETicaretDersiProje.Eticaret.MvcWebUI.Controllers
             return RedirectToAction("Index");
         }
 
-   
+
+        public JsonResult SendMailToUser(string mesaj,string baslik)
+        {
+            var result = false;
+            result = SendEmail("yunus.unver1995@gmail.com",baslik , mesaj);
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public bool SendEmail(string toEmail, string subject, string emailBody)
+        {
+            try
+            {
+                string senderEmail = System.Configuration.ConfigurationManager.AppSettings["SenderEmail"].ToString();
+                string senderPassword = System.Configuration.ConfigurationManager.AppSettings["SenderPassword"].ToString();
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.Timeout = 100000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(senderEmail, senderPassword);
+
+                MailMessage mailMessage = new MailMessage(senderEmail, toEmail, subject, emailBody);
+                mailMessage.IsBodyHtml = true;
+                mailMessage.BodyEncoding = UTF8Encoding.UTF8;
+
+                client.Send(mailMessage);
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+
     }
 }
